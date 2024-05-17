f1353520012b8482265792a156cb14059709be76
Devjabli <aroussiotmane27@gmail.com>
2024-05-17 23:37:44 +0100

    engine tests-> method test_all

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+"""
+
+import unittest
+from datetime import datetime
+from uuid import uuid4
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    """
+    """
+    def test_all(self):
+        """
+        """
+        storage = FileStorage()
+        self.assertEqual(storage.all(), FileStorage._FileStorage__objects)
+        obj = BaseModel()
+        obj.id = str(uuid4)
+        storage.new(obj)
+        self.assertIn(f"BaseModel.{obj.id}", storage.all())
+    
+if __name__ == '__main__':
+    unittest.main()
