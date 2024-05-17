2d7ba66cc87b5c9dae3b1a904cabc18be40bcc18
Devjabli <aroussiotmane27@gmail.com>
2024-05-17 23:29:42 +0100

    method tests-> base_models -> to_dict - str

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -30,6 +30,23 @@ class TestBaseModel(unittest.TestCase):
         time_created = obj.created_at
         obj.save()
         self.assertNotEqual(obj.updated_at, time_created)
+    
+    def test_to_dict(self):
+        """"""
+        obj = BaseModel()
+        o_dict = obj.to_dict()
+        self.assertTrue(isinstance(o_dict, dict))
+        self.assertEqual(len(o_dict), 4)
+        self.assertTrue(isinstance(o_dict['created_at'], str))
+        self.assertTrue(isinstance(o_dict['updated_at'], str))
+        self.assertEqual(o_dict['__class__'], 'BaseModel')
+
+    def test_str(self):
+        """"""
+        obj = BaseModel()
+        o_str = str(obj)
+        str_printed = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(o_str, str_printed)
 
 
 if __name__ == '__main__':
