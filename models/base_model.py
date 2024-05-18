f250d0de8f4818fff8d23e458ebbf3df8b85f4e5
Devjabli <aroussiotmane27@gmail.com>
2024-05-18 19:39:59 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 """ Representing the BaseModel class. """
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
@@ -20,7 +20,7 @@ class BaseModel:
         """
 
         if not kwargs:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
