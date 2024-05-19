8485b50edf212d43954356c84616f9df494bc3c5
Devjabli <aroussiotmane27@gmail.com>
2024-05-19 23:45:51 +0100

    base_models_test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,51 +7,72 @@ test_save
 test_to_dict
 """
 
+
 import unittest
 from datetime import datetime
+import uuid
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """ Testing for all method from BaseModel"""
+    def setUp(self):
+        """
+        Sets up the test methods
+        """
+        self.instance = BaseModel()
+        self.instance_with_kwargs = BaseModel(id=str(uuid.uuid4()), created_at=datetime.today().isoformat(), updated_at=datetime.today().isoformat())
 
-    def test_init(self):
-        """ Testing initializer Method"""
+    def tearDown(self):
+        """
+        Clean up after test
+        """
+        del self.instance
+        del self.instance_with_kwargs
 
-        obj = BaseModel()
-        sizes = len(obj.id)
-        self.assertIsInstance(obj.id, str)
-        self.assertIsInstance(obj.created_at, datetime)
-        self.assertIsInstance(obj.updated_at, datetime)
-        self.assertEqual(sizes, 36)
-        self.assertNotEqual(obj.created_at, obj.updated_at)
+    def test_instance_creation(self):
+        """
+        Test creation of a new instance of BaseModel
+        """
+        self.assertIsInstance(self.instance, BaseModel)
+        self.assertIsInstance(self.instance.id, str)
+        self.assertIsInstance(self.instance.created_at, datetime)
+        self.assertIsInstance(self.instance.updated_at, datetime)
 
-    def test_save(self):
-        """ Testing method save for serializer data"""
+    def test_instance_creation_with_kwargs(self):
+        """
+        Test creation of a new instance of BaseModel with kwargs
+        """
+        self.assertIsInstance(self.instance_with_kwargs, BaseModel)
+        self.assertIsInstance(self.instance_with_kwargs.id, str)
+        self.assertEqual(self.instance_with_kwargs.id, self.instance_with_kwargs.id)
+        self.assertIsInstance(self.instance_with_kwargs.created_at, datetime)
+        self.assertIsInstance(self.instance_with_kwargs.updated_at, datetime)
 
-        obj = BaseModel()
-        time_created = obj.created_at
-        obj.save()
-        self.assertNotEqual(obj.updated_at, time_created)
+    def test_save_method(self):
+        """
+        Test the save method of the BaseModel
+        """
+        old_updated_at = self.instance.updated_at
+        self.instance.save()
+        self.assertNotEqual(self.instance.updated_at, old_updated_at)
 
-    def test_to_dict(self):
-        """ Testing the method to_dict """
+    def test_to_dict_method(self):
+        """
+        Test the to_dict method of the BaseModel
+        """
+        instance_dict = self.instance.to_dict()
+        self.assertEqual(instance_dict["__class__"], "BaseModel")
+        self.assertEqual(instance_dict["id"], self.instance.id)
+        self.assertEqual(instance_dict["created_at"], self.instance.created_at.isoformat())
+        self.assertEqual(instance_dict["updated_at"], self.instance.updated_at.isoformat())
 
-        obj = BaseModel()
-        o_dict = obj.to_dict()
-        self.assertTrue(isinstance(o_dict, dict))
-        self.assertEqual(len(o_dict), 4)
-        self.assertTrue(isinstance(o_dict['created_at'], str))
-        self.assertTrue(isinstance(o_dict['updated_at'], str))
-        self.assertEqual(o_dict['__class__'], 'BaseModel')
+    def test_str_method(self):
+        """
+        Test the __str__ method of the BaseModel
+        """
+        string_rep = str(self.instance)
+        expected_string = f"[BaseModel] ({self.instance.id}) {self.instance.__dict__}"
+        self.assertEqual(string_rep, expected_string)
 
-    def test_str(self):
-        """ Testing str method for returning str presentation """
-        obj = BaseModel()
-        o_str = str(obj)
-        str_printed = f"[BaseModel] ({obj.id}) {obj.__dict__}"
-        self.assertEqual(o_str, str_printed)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
