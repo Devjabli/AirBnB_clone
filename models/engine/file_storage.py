d27fe81461dcd2a16d7e1772fb22f9c0fade7275
Devjabli <aroussiotmane27@gmail.com>
2024-05-20 18:40:30 +0100

    fixint pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Defining FileStorage model to manipulate data to json
+Defining FileStorage model to manipulate data to json.
 """
+
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
@@ -14,13 +16,12 @@ from models.state import State
 
 class FileStorage:
     """
-    Representing a storage engine
+    Represents a storage engine for saving and loading data in JSON format.
 
-    Attributes private:
-        __file_path (str): where objects saved to this file.
-        __objects (dict): dictionary of objects.
+    Attributes (private):
+        __file_path (str): Path to the file where objects are saved.
+        __objects (dict): Dictionary of objects currently in storage.
     """
-
 
     __file_path = "file.json"
     __objects = {}
@@ -34,30 +35,51 @@ class FileStorage:
         "State": State
     }
 
-    def all(self):
-        """ Returning the dictionary __objects"""
+    def all(self) -> dict:
+        """
+        Returns the dictionary __objects.
+        """
         return self.__objects
 
-    def new(self, obj):
-        """ Sets in __objects obj with key id"""
+    def new(self, obj: BaseModel) -> None:
+        """
+        Adds a new object to __objects with a key formatted as <class name>.<id>.
+
+        Args:
+            obj (BaseModel): The object to be added.
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
-    def save(self):
-        """ Serializing data __objects to JSON __file_path """
+    def save(self) -> None:
+        """
+        Serializes __objects to a JSON file specified by __file_path.
+        """
         with open(self.__file_path, 'w', encoding='utf-8') as f:
-            json.dump({key: value.to_dict() for key, value in self.__objects.items()}, f)
+            json.dump(
+                {key: value.to_dict() for key, value in self.__objects.items()},
+                f, indent=4
+            )
 
-    def reload(self):
-        """ Deserlializing JSON file from __file_path to __objects as dictionary"""
-        try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
-                objdict = json.load(f)
-                for obj_data in objdict.values():
-                    class_name = obj_data["__class__"]
-                    if class_name in self.class_map:
-                        cls = self.class_map[class_name]
-                        del obj_data["__class__"]
-                        self.new(cls(**obj_data))
-        except FileNotFoundError:
-            pass
+    def reload(self) -> None:
+        """
+        Deserializes the JSON file specified by __file_path to __objects.
+
+        If the file does not exist, it does nothing.
+        """
+        if os.path.exists(self.__file_path):
+            try:
+                with open(self.__file_path, 'r', encoding='utf-8') as f:
+                    objdict = json.load(f)
+                    for obj_data in objdict.values():
+                        class_name = obj_data.get("__class__")
+                        if class_name in self.class_map:
+                            cls = self.class_map[class_name]
+                            obj_data.pop("__class__", None)
+                            self.new(cls(**obj_data))
+            except json.JSONDecodeError:
+                print("Error decoding JSON file. File might be corrupted.")
+            except Exception as e:
+                print(f"Unexpected error occurred: {e}")
+        else:
+            print("No file found to reload data.")
