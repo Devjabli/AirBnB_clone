cbc736087a57557f2aacfff7febc84dabb873d3f
Devjabli <aroussiotmane27@gmail.com>
2024-05-20 12:14:23 +0100

    update class tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for User class
+"""
+import os
+import unittest
+from datetime import datetime
+from time import sleep
+from models.user import User
+import models
+
+
+class TestCity(unittest.TestCase):
+    """Tests for User class instantiation"""
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
